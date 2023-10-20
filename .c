d35b696685912c89400bd06edd8ee46dba1eb1a4
Kelvin Moreira <kelvin20mso@gmail.com>
2023-10-20 06:37:09 -0300

    Teste da biblioteca
    código de teste usando a biblioteca xlsxwriter.h.

---
+++ .c
@@ -0,0 +1,37 @@
+
+/*
+#include <stdio.h>
+#include "xlsxwriter.h"
+
+int row, col;
+
+typedef struct {
+    char name[32];
+    unsigned int age;
+    char cpf[12];
+    char rg[13];
+} People;
+
+int
+
+main(int argc, *char argv[]) {
+    lxw_workbook *workbook = workbook_new("Planilha_Teste.xlsx");
+    lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
+    row = col = 0;
+    worksheet_write_string(worksheet, row, col, "hello world!", NULL);
+    workbook_close(workbook);
+    return 0;
+}
+
+int format_cpf(void) {
+    int i;
+    if for(i = 0; i = 3 || i = 6) {
+        printf(".");
+    }
+    else for (i = 0; i > 6) {
+        printf("-");
+    }
+    getch()
+    return 0;
+}
+*/
